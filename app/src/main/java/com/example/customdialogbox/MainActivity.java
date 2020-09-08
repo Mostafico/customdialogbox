89d293945409cfca4de2237fa333930ac42e5761
mostafico <mostafico@yahoo.com>
2020-09-08 21:30:04 +0200

    Added custom dialog

--- app/src/main/java/com/example/customdialogbox/MainActivity.java
+++ app/src/main/java/com/example/customdialogbox/MainActivity.java
@@ -1,13 +1,48 @@
 package com.example.customdialogbox;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button acceptBtn = findViewById(R.id.btn_accept);
+        Button rejectBtn = findViewById(R.id.btn_reject);
+
+        acceptBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                ViewGroup viewGroup = findViewById(android.R.id.content);
+                View dialogView = LayoutInflater.from(v.getContext()).inflate(R.layout.custom_view, viewGroup, false);
+                builder.setView(dialogView);
+                Button btn = dialogView.findViewById(R.id.buttonOk);
+
+                final AlertDialog alertDialog = builder.create();
+                alertDialog.show();
+
+                btn.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        alertDialog.dismiss();
+                    }
+                });
+            }
+        });
+
+        rejectBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
